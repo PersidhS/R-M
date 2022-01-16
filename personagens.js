17f48089279be2fb1ef77b098b0d97eab88200f8
PersidhS <persidh.cris@gmail.com>
2022-01-16 17:12:11 -0300

    criando uma função que reunirá personagens escolhidos e declarando que o botão gerará a ação

--- personagens.js
+++ personagens.js
@@ -3,3 +3,10 @@ const botao = document.querySelector('button');
 gerarvalorAleatorio = () => {
     return Math.floor(Math.random() * 671);
 }
+
+function pegarPersonagens() {
+    pegarPersonagem1();
+    pegarPersonagem2();
+    pegarPersonagem3();
+}
+botao.onclick = pegarPersonagens;
